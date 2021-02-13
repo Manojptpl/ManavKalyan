895e029184ea98bb47b4b4489b05ce14cba2c66d
Manoj Kumar <manojkchaurasia075@yahoo.com>
2021-02-13 14:01:07 +0530

    ManavKalyan Web Application

---
+++ ManavKalyanSevaSamiti/ManavKalyanSevaSamiti/Controllers/HomeController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ManavKalyanSevaSamiti.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult PhotoGallery()
+        {
+            return View();
+        }
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
+        public ActionResult RotatePhotoGallery()
+        {
+            return View();
+        }        
+        public ActionResult TextRotate()
+        {
+            return View();
+        }
+        public ActionResult PhotoRotate()
+        {
+            return View();
+        }
+        public ActionResult InfiniteImageLoop()
+        {
+            return View();
+        }
+        public ActionResult ImageGalleryZoom()
+        {
+            return View();
+        }
+        public ActionResult CardRotate()
+        {
+            return View();
+        }
+        public ActionResult PresidentProfile()
+        {
+            return View();
+        }
+        public ActionResult PhotoCovers()
+        {
+            return View();
+        }
+        public ActionResult ImageReflection()
+        {
+            return View();
+        }  
+        public ActionResult Perspective3DMouseOver()
+        {
+            return View();
+        }
+        public ActionResult GlassEffect()
+        {
+            return View();
+        }
+        public ActionResult ImageHover()
+        {
+            return View();
+        }
+        public ActionResult DashBoard()
+        {
+            return View();
+        }
+        public ActionResult Accordian()
+        {
+            return View();
+        }
+    }
+}
---
+++ ManavKalyanSevaSamiti/ManavKalyanSevaSamiti/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ManavKalyanSevaSamiti.MvcApplication" Language="C#" %>
